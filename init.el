6113f925a7386f3974dd64d7148c371ca788fd05
Roman Nakutnyi <rnakutnyi@jerasoft.net>
2019-05-25 09:46:21 +0300

    Disable auto-indent for yanked text for now

--- init.el
+++ init.el
@@ -48,20 +48,20 @@
 (add-to-list 'package-archives
   '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
-;auto-indent yanked code
-(dolist (command '(yank yank-pop))
-   (eval `(defadvice ,command (after indent-region activate)
-            (and (not current-prefix-arg)
-                 (member major-mode '(emacs-lisp-mode lisp-mode
-                                                      clojure-mode    scheme-mode
-                                                      haskell-mode    ruby-mode
-                                                      rspec-mode      python-mode
-                                                      c-mode          c++-mode
-                                                      objc-mode       latex-mode
-                                                      plain-tex-mode))
-                 (let ((mark-even-if-inactive transient-mark-mode))
-                   (indent-region (region-beginning) (region-end) nil))))))
-
+;;auto-indent yanked code
+;(dolist (command '(yank yank-pop))
+;   (eval `(defadvice ,command (after indent-region activate)
+;            (and (not current-prefix-arg)
+;                 (member major-mode '(emacs-lisp-mode lisp-mode
+;                                                      clojure-mode    scheme-mode
+;                                                      haskell-mode    ruby-mode
+;                                                      rspec-mode      python-mode
+;                                                      c-mode          c++-mode
+;                                                      objc-mode       latex-mode
+;                                                      plain-tex-mode))
+;                 (let ((mark-even-if-inactive transient-mark-mode))
+;                   (indent-region (region-beginning) (region-end) nil))))))
+;
 ;Set keys
 (global-set-key (kbd "C-c t") 'bookmark-insert)
 (global-set-key (kbd "C-x C-b") 'ibuffer)
